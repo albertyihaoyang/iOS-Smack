656559c725eca9628e00183f199bc4d065f5ead9
杨以皓 <yihao@yyh-MBP.local>
2017-12-11 01:25:00 -0800

    2nd

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by 杨以皓 on 12/11/17.
+//  Copyright © 2017 yyh. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closePress(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by 杨以皓 on 12/11/17.
+//  Copyright © 2017 yyh. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
